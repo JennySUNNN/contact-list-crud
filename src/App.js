14eb2c57729d3553101d31d87607471b6535219d
Jenny Sun <sunjiani0821@outlook.com>
2023-03-22 14:31:00 -0400

    construct main page layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
+import { Link } from "react-router-dom";
+import { IoMdPersonAdd } from "react-icons/io";
+import ContactList from "./componenets/ContactList";
 
 function App() {
   return (
     <div className="App">
+      {/* Header Section */}
 
+      <div className="flex items-center justify-between py-14 px-28">
+        <h1 className="font-bold text-[40px]">Contacts</h1>
+        {/* Add New Contact Button */}
+        <Link
+          to={"/new"}
+          className="flex items-center gap-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl"
+        >
+          Add Contact
+          <IoMdPersonAdd size={"18px"} />
+        </Link>
+      </div>
+
+      {/* Contact List Section */}
+      <ContactList />
+      
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        firaMono: ["Fira Mono", "monospace"],
+        playfairDisplay: ["Playfair Display", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+        yesevaOne: ["Yeseva One", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
