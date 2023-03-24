91f75d7b1b56b0fce6efaab35ce20d01542c0630
Jenny Sun <67336024+JennySUNNN@users.noreply.github.com>
2023-03-24 21:55:41 +0800

    fixed phone input default value

--- src/componenets/EditContactForm.jsx
+++ src/componenets/EditContactForm.jsx
@@ -68,7 +68,7 @@ const EditContactForm = () => {
               type="tel"
               name="phone"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              defaultValue={contact.email}
+              defaultValue={contact.phone}
               required
             />
           </div>
